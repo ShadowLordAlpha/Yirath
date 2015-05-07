fc6b1162fe8f3339457efcc5c1db3de29b9efaa4
ShadowLordAlpha <shadow.alpha70@gmail.com>
2015-05-06 18:00:49 -0700

    Music stuff / play stuff
    added the music file as well as started work on the play state

--- src/main/java/yirath/state/PlayState.java
+++ src/main/java/yirath/state/PlayState.java
@@ -1,7 +1,38 @@
 package yirath.state;
 
 import com.shc.silenceengine.core.GameState;
+import com.shc.silenceengine.graphics.Batcher;
 
 public class PlayState extends GameState {
+
+	@Override
+	public void onEnter() {
+		// TODO Auto-generated method stub
+		super.onEnter();
+	}
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+		super.update(delta);
+	}
+
+	@Override
+	public void render(float delta, Batcher batcher) {
+		// TODO Auto-generated method stub
+		super.render(delta, batcher);
+	}
+
+	@Override
+	public void resize() {
+		// TODO Auto-generated method stub
+		super.resize();
+	}
+
+	@Override
+	public void onLeave() {
+		// TODO Auto-generated method stub
+		super.onLeave();
+	}
 
 }
